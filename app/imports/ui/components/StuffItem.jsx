cab03eb8610814aac5a4518742100b3bd6bbdd51
Derealization-kk <bingying.li02@gmail.com>
2023-10-21 21:44:46 -1000

    Time:24.40 Date:10/21

--- app/imports/ui/components/StuffItem.jsx
+++ app/imports/ui/components/StuffItem.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-
+import { Button } from 'react-bootstrap';
+import { Trash } from 'react-bootstrap-icons';
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
-const StuffItem = ({ stuff }) => (
-  <tr>
-    <td>{stuff.name}</td>
-    <td>{stuff.quantity}</td>
-    <td>{stuff.condition}</td>
-    <td>
-      <Link to={`/edit/${stuff._id}`}>Edit</Link>
-    </td>
-  </tr>
-);
+// eslint-disable-next-line react/prop-types
+const StuffItem = ({ stuff, collection }) => {
+  const removeItem = (docID) => {
+    // eslint-disable-next-line no-console
+    console.log(`The item to remove is ${docID}`);
+    // eslint-disable-next-line react/prop-types
+    collection.remove(docID);
+  };
+
+  return (
+    <tr>
+      <td>{stuff.name}</td>
+      <td>{stuff.quantity}</td>
+      <td>{stuff.condition}</td>
+      <td>
+        <Link to={`/edit/${stuff._id}`}>Edit</Link>
+      </td>
+      <td><Button variant="danger" onClick={() => removeItem(stuff._id)}><Trash /></Button></td>
+    </tr>
+  );
+};
 
 // Require a document to be passed to this component.
 StuffItem.propTypes = {
@@ -22,6 +34,8 @@ StuffItem.propTypes = {
     condition: PropTypes.string,
     _id: PropTypes.string,
   }).isRequired,
+  // eslint-disable-next-line react/forbid-prop-types
+  collection: PropTypes.object.isRequired,
 };
 
 export default StuffItem;
